0ab4bb2d2b1c9e739579a68b10d9081bed0566fe
Nayak <jitu.nayak@kochgs.com>
2021-12-19 12:54:14 +0530

    Customized bottom navigation bar

--- screens/AppNavigator.js
+++ screens/AppNavigator.js
@@ -2,22 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-
-import { Layout, Text, Icon, Divider } from "@ui-kitten/components";
 import HomeScreen from "./HomeScreen";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import CartScreen from "./CartScreen";
 import OrdersScreen from "./OrdersScreen";
+import { Layout, Text, Divider } from "@ui-kitten/components";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
-
-const PersonIcon = (props) => <Icon {...props} name="person-outline" />;
-
-const BellIcon = (props) => <Icon {...props} name="bell-outline" />;
-
-const EmailIcon = (props) => <Icon {...props} name="email-outline" />;
 
 const UsersScreen = () => (
   <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -25,28 +19,42 @@ const UsersScreen = () => (
   </Layout>
 );
 
-const BottomTabBar = ({ navigation, state }) => (
-  <>
-    <SafeAreaView edges={["bottom"]}>
-      <BottomNavigation
-        selectedIndex={state.index}
-        onSelect={(index) => navigation.navigate(state.routeNames[index])}
-      >
-        <BottomNavigationTab title="USERS" icon={PersonIcon} />
-        <BottomNavigationTab title="ORDERS" icon={BellIcon} />
-        <BottomNavigationTab title="SUBSCRIPTION" icon={EmailIcon} />
-      </BottomNavigation>
-    </SafeAreaView>
-  </>
-);
-
 const TabNavigator = ({ navigation }) => (
   <Tab.Navigator
-    screenOptions={{
+    screenOptions={({ route }) => ({
       headerShown: false,
-    }}
+      tabBarLabel: ({ focused, color }) => {
+        return (
+          <Text
+            style={{
+              color,
+              fontSize: 12,
+              paddingBottom: 4,
+            }}
+          >
+            {route.name}
+          </Text>
+        );
+      },
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === "Home") {
+          iconName = focused ? "ios-home" : "ios-home-outline";
+        } else if (route.name === "Subscription") {
+          iconName = focused ? "ios-wallet" : "ios-wallet-outline";
+        } else if (route.name === "Orders") {
+          iconName = focused ? "list-circle" : "list-circle-outline";
+        }
+        // You can return any component that you like here!
+        return (
+          <Layout style={{ padding: 0 }}>
+            <Ionicons name={iconName} size={26} color={color} />
+          </Layout>
+        );
+      },
+    })}
   >
-    <Tab.Screen name="Gin Delivery 2" component={HomeScreen} />
+    <Tab.Screen name="Home" component={HomeScreen} />
     <Tab.Screen name="Orders" component={OrdersScreen} />
     <Tab.Screen name="Subscription" component={UsersScreen} />
   </Tab.Navigator>
@@ -56,12 +64,12 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="HomeStack"
         screenOptions={{
           headerShown: true,
         }}
       >
-        <Stack.Screen name="Home" component={TabNavigator} />
+        <Stack.Screen name="HomeStack" component={TabNavigator} />
         <Stack.Screen name="Order" component={CartScreen} />
       </Stack.Navigator>
     </NavigationContainer>
